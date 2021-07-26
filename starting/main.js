fa692b9b4baeecc2412862212b54f6f1cb7d28ae
ChrispyBro2113 <cobz1913@gmail.com>
2021-07-25 19:25:26 -0500

    finished game

--- starting/main.js
+++ starting/main.js
@@ -5,63 +5,142 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+let currentlyPlaying = true;
 
 class Field {
-    construct(hatAndHoles, field){
+    constructor(field) {
         this._field = field;
-        this._hatAndHoles = hatAndHoles;
+        this.y = 0;
+        this.x = 0;
     }
 
-    playGame(){
-        let x = 0;
-        let y = 0;
-        this.print();
+    get field() {
+        return this._field;
+    }
 
-        while (this._hatAndHoles[y][x] === pathCharacter || this._hatAndHoles[y][x] === fieldCharacter){
-            const direction = prompt('Which direction would you like to move? Please enter N for North, S for South, E for East, or W for West. \n');
-            
-            if(direction.toLowerCase() === 'n'){
-                if( y === 0){
-                    console.log('You cannot move any further North. Please choose another direction');
-                }else{
-                    y -= 1;
-                }
-            }else if(direction.toLowerCase() === 's'){
-                if(y >= this._field.length){
-                    console.log('You cannot move any further South. Please choose another direction');
-                }else{
-                    y += 1;
-                }
-            }else if(direction.toLowerCase() === 'w'){
-                if(x === 0){
-                    console.log('You cannot move any further West. Please choose another direction')
-                }else{
-                    x -= 1;
-                }
-            }else if(direction.toLowerCase() === 'e'){
-                if(x >= this._field[y].length){
-                    console.log('You cannot move any further East. Please choose another direction');
-                }else{
-                    x += 1;
-                }
-            }else{
-                console.log('Invalid entry. Please enter N, S, E, or W');
-            }
+    //Print the field to the terminal in a two-dimensional plane
+    print() {
+        return this.field.map(row =>
+            row.join('')
+        ).join('\n');
+    }
 
-            if(this._hatAndHoles[y][x] == hat){
-                console.log('You found the hat! You Win!');
-            }else if(this._hatAndHoles[y][x] === hole){
-                console.log('You fell in a hole. Game Over');
-            }else{
-                this._field[y][x] = pathCharacter;
-                this.print(this._field);
+    //Retrieve user's input and move the player's cursor
+    ask() {
+        let move = prompt('Which direction do you want to move to? (u for Up, d for down, l for left and r for right)');
+        switch(move.toLowerCase()) {
+            case 'u':
+                console.log('Moving up');
+                this.y -= 1;
+                break;
+            case 'd':
+                console.log('Moving down');
+                this.y += 1;
+                break;
+            case 'l':
+                console.log('Moving left');
+                this.x -= 1;
+                break;
+            case 'r':
+                console.log('Moving right');
+                this.x += 1;
+                break;
+            default:
+                break;
+        }    
+    }
+
+    //Detect the game status if player wins or loses
+    checkWin() {
+        //The game will crash if trying to read an out of range array. For example: this.field[-1][0], so this if condition is trying to capture the error.
+        //However, this.field[0][-1] will return 'undefined' which will be capture in below switch/case
+        if (this.field[this.y] == undefined) {
+            console.log('You lose - Out of boundary');
+            return currentlyPlaying = false;            
+        }
+        //
+        switch (this.field[this.y][this.x]) {
+            case hole:
+                console.log('You lose - You fell in a hole!');
+                currentlyPlaying = false;
+                break;
+            case undefined:
+                console.log('You lose - Out of boundary');
+                currentlyPlaying = false;
+                break;
+            case hat:
+                console.log('You win - You found the hat!');
+                currentlyPlaying = false;
+                break;
+            case fieldCharacter:
+                console.log('Keep looking for the hat...');
+                this.field[this.y][this.x] = pathCharacter;
+                break;
+            case pathCharacter:
+                console.log('You are stepping on *');
+                break;
+        }    
+    }
+
+    static generateField(height, width, percentage) {
+
+        //Helper function to return hole or fieldCharacter depening on percentage.
+        const fieldOrHole = (percentage) => {
+            if (percentage >= 0 && percentage <= 100) {
+              const ranNum = Math.random() * 100;
+              if (ranNum < percentage) {
+                return hole;
+              } else {
+                return fieldCharacter;
+              }
+            } else {
+              console.log('Please enter a number between 0 - 100');
             }
         }
+
+        ////Helper function to return a plain field with no hat and pathCharacter
+        const plainField = () => {
+            function makeWidthArray() {
+                let widthArray = [];
+                for (let i=0; i < width; i++) {
+                    widthArray.push(fieldOrHole(percentage));
+                }
+                return widthArray;
+            }
+            let plainField = [];
+            for (let i=0; i < height; i++) {
+                plainField.push(makeWidthArray());
+            }
+            return plainField;
+        }
+
+        const gameReadyField = plainField();
+
+        //Adding hat on gameReadyField, while loop will check if hat sits on * and will reposition if so
+        do {
+            gameReadyField[Math.floor(Math.random() * height)][Math.floor(Math.random() * width)] = hat;
+        }   while (gameReadyField[0][0] == hat);
+        
+        //Adding pathCharacter to left-upper corner
+        gameReadyField[0][0] = pathCharacter;
+
+        return gameReadyField;
     }
 
-    print(){
-        for(let row of this.field){
-            console.log(row.join(' '));
-        }
-    }
 }
+
+//Generating a new randomized field into "newField" and will insert to "myField" below:
+//generateField() takes 3 parameters. First is the y-axis, second is x-axis and third id the percentage of holes in the field(Please enter between 0 - 100).
+
+const myField = new Field(Field.generateField(10,10,30));
+
+function game() {
+    while(currentlyPlaying) {
+        console.log(myField.print());
+        myField.ask();
+        myField.checkWin();
+    }
+    console.log('Game Over!');
+}
+
+game();
